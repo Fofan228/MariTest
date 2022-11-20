ff619519a961b59c17dc9bf0ac7b0a34000563d4
Nanoster1 <danil2002.11@mail.ru>
2022-11-20 15:09:05 +0500

    Added Repositories

--- Mari.Infrastructure/DependencyInjection.cs
+++ Mari.Infrastructure/DependencyInjection.cs
@@ -28,6 +28,7 @@ public static class DependencyInjection
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IReleaseRepository, ReleaseRepository>();
+        services.AddScoped<ICommentRepository, CommentRepository>();
 
         return services;
     }
---
+++ Mari.Infrastructure/Persistence/Repositories/CommentRepository.cs
@@ -0,0 +1,14 @@
+using Mari.Application.Common.Interfaces.Persistence;
+using Mari.Domain.Comments;
+using Mari.Domain.Comments.ValueObjects;
+using Mari.Infrastructure.Persistence.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mari.Infrastructure.Persistence.Repositories;
+
+public class CommentRepository : Repository<Comment, CommentId>, ICommentRepository
+{
+    public CommentRepository(MariDbContext context) : base(context)
+    {
+    }
+}
