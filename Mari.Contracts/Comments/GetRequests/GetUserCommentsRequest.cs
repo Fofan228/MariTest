68eb9019100d31dc625e6199ba93b32bdaa6e391
Seraphic <danilsocol-2002@mail.ru>
2022-12-08 22:16:56 +0500

    Comment Requests and Responce + Fix

---
+++ Mari.Contracts/Comments/GetRequests/GetUserCommentsRequest.cs
@@ -0,0 +1,19 @@
+﻿using Mari.Contracts.Comments.Responces;
+using Mari.Contracts.Common.Routes.Server;
+using Mari.Http.Common.Classes;
+using Mari.Http.Requests;
+
+namespace Mari.Contracts.Comments.GetRequests;
+
+public class GetUserCommentsRequest : GetRequest<IEnumerable<CommentResponse>>
+{
+
+    public const string ConstRouteTemplate = $"{ServerRoutes.Controllers.Comment}/user/{{{nameof(Route.ReleaseId)}}}";
+    public override string RouteTemplate => ConstRouteTemplate;
+    
+    public GetUserCommentsRequest(Route route) : base(route: route)
+    {
+    }
+
+    public record Route(Guid ReleaseId) : RequestRoute;
+}
---
+++ Mari.Contracts/Comments/PatchRequests/UpdateMessageRequest.cs
@@ -0,0 +1,23 @@
+﻿using Mari.Contracts.Common.Routes.Server;
+using Mari.Http.Common.Classes;
+using Mari.Http.Models;
+using Mari.Http.Requests;
+
+namespace Mari.Contracts.Comments.PatchRequests;
+
+public class UpdateMessageRequest : PatchRequest<VoidResponse>
+{
+    public const string ConstRouteTemplate = $"{ServerRoutes.Controllers.Comment}";
+    public override string RouteTemplate => ConstRouteTemplate;
+
+    public UpdateMessageRequest(Body body) : base(body: body)
+    {
+    }
+    
+    public record Body(
+        Guid CommentId,
+        string Message,
+        DateTime CreateDate,
+        bool IsEdit
+    ):RequestBody;
+}
